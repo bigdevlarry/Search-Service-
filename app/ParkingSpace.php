a12e1e2ab0f57f571bf8dbae453a305c7895c9e7
larry.sulebalogun <larry.sulebalogun@tillo.io>
2025-04-27 19:08:22 +0100

    Added middleware, factories, providers and model class

---
+++ app/ParkingSpace.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class ParkingSpace
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $lat
+ * @property float $lng
+ * @property string $space_details
+ * @property string $city
+ * @property string $street_name
+ * @property int $no_of_spaces
+ * @property-read User $owner
+ */
+class ParkingSpace extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
