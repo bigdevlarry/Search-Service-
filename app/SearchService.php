020c0081fa08b30cabe7b63874e43cb8ee0b0be5
larry.sulebalogun <larry.sulebalogun@tillo.io>
2025-04-27 20:36:56 +0100

    Renamed parameters to improve readibility

--- app/SearchService.php
+++ app/SearchService.php
@@ -7,32 +7,32 @@ class SearchService
     const WGS84_A = 6378137.0; // Major semiaxis
     const WGS84_B = 6356752.3; // Major semiaxis
 
-    public function getBoundingBox(float $lat, float $lng, int $radius): array
+    public function getBoundingBox(float $latitude, float $longitude, int $radius): array
     {
-        $lat = deg2rad($lat);
-        $lng = deg2rad($lng);
+        $latitude = deg2rad($latitude);
+        $longitude = deg2rad($longitude);
         $halfSide = 1000 * $radius;
 
-        $radius = $this->calculateWGS84EarthRadius($lat);
-        $pRadius = $radius * cos($lat);
+        $radius = $this->calculateWGS84EarthRadius($latitude);
+        $pRadius = $radius * cos($latitude);
 
         return [
-            'se_lat' => rad2deg($lat - $halfSide/$radius),
-            'nw_lat' => rad2deg($lat + $halfSide/$radius),
-            'nw_lng' => rad2deg($lng - $halfSide/$pRadius),
-            'se_lng' => rad2deg($lng + $halfSide/$pRadius),
+            'se_lat' => rad2deg($latitude - $halfSide/$radius),
+            'nw_lat' => rad2deg($latitude + $halfSide/$radius),
+            'nw_lng' => rad2deg($longitude - $halfSide/$pRadius),
+            'se_lng' => rad2deg($longitude + $halfSide/$pRadius),
         ];
     }
 
     /**
      * Calculate Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
      */
-    private function calculateWGS84EarthRadius(float $lat): float
+    private function calculateWGS84EarthRadius(float $latitude): float
     {
-        $An = self::WGS84_A * self::WGS84_A * cos($lat);
-        $Bn = self::WGS84_B * self::WGS84_B * sin($lat);
-        $Ad = self::WGS84_A * cos($lat);
-        $Bd = self::WGS84_B * sin($lat);
+        $An = self::WGS84_A * self::WGS84_A * cos($latitude);
+        $Bn = self::WGS84_B * self::WGS84_B * sin($latitude);
+        $Ad = self::WGS84_A * cos($latitude);
+        $Bd = self::WGS84_B * sin($latitude);
         return sqrt(($An * $An + $Bn * $Bn) / ($Ad * $Ad + $Bd * $Bd));
     }
 }
